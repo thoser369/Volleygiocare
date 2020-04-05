096a06055e543ff5df3e512474e90c8dcef25fca
thoser369 <38078706+thoser369@users.noreply.github.com>
2020-04-05 06:49:09 +0200

    modifiche varie

--- VolleyGiocare2.0/src/app/pages/giocatori/giocatori.module.ts
+++ VolleyGiocare2.0/src/app/pages/giocatori/giocatori.module.ts
@@ -1,23 +1,26 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import {IonicModule} from '@ionic/angular';
 
-import {GiocatoriPageRoutingModule} from './giocatori-routing.module';
-
 import {GiocatoriPage} from './giocatori.page';
 import {TranslateModule} from '@ngx-translate/core';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const routes: Routes = [
+    {
+        path: '',
+        component: GiocatoriPage
+    }
+];
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
-        GiocatoriPageRoutingModule,
-        TranslateModule,
-        ReactiveFormsModule
+        RouterModule.forChild(routes),
+        TranslateModule
     ],
     declarations: [GiocatoriPage],
 
